4fff3b3884da760030cd95805a64a32d06f5bf13
Yidingluo <s3713960@student.rmit.edu.au>
2021-08-14 16:27:56 +0800

    Create DepositSlotTimedTestAdvancedTimingS33713960.java

---
+++ src/test/java/au/edu/rmit/ct/DepositSlotTimedTestAdvancedTimingS33713960.java
@@ -0,0 +1,47 @@
+/**
+*  
+* Name: Yiding Luo
+* Student ID: s3713960
+* Email: s3713960@student.rmit.edu.au
+* 
+* [OPTIONAL: add any notes here about the code]
+*/
+
+package au.edu.rmit.ct;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Please replace S3214321 of class name with your own student number
+ */
+
+class DepositSlotTimedTestAdvancedTimingS33713960 {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
